07c5d8b1afe3742c76b479c8f48fd16854d3264f
Javier Lopez Cano <javier.lopezcano@estudiante.uam.es>
2018-11-16 14:05:28 +0100

    ej2

--- Practica3/Ej2.sh
+++ Practica3/Ej2.sh
@@ -2,9 +2,9 @@
 
 # Inicializar variables
 Npaso=64
-P=10
-Ninicio=$((2000+1024*$P))
-Nfinal=$((2000+1024*($P+1)))
+P=1
+Ninicio=$((100+1024*$P))
+Nfinal=$((100+1024*($P+1)))
 fDAT1024=cache_1024.dat
 fDAT2048=cache_2048.dat
 fDAT4096=cache_4096.dat
@@ -42,45 +42,26 @@ echo "Running slow and fast..."
 		slow2048=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow2048.dat --I1=2048,1,64 --D1=2048,1,64 --LL=8388608,1,64 ./slow $N)
 		slow4096=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow4096.dat --I1=4096,1,64 --D1=4096,1,64 --LL=8388608,1,64 ./slow $N)
 		slow8192=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow8192.dat --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 ./slow $N)
-
-    slowResults1024[$j]=$(cg_annotate cgout_slow1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
-		slowResults2048[$j]=$(cg_annotate cgout_slow2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
-		slowResults4096[$j]=$(cg_annotate cgout_slow4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
-		slowResults8192[$j]=$(cg_annotate cgout_slow8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
-
-	done
-
-	for ((N = Ninicio ; N <= Nfinal ; N += Npaso)); do
-		echo "Fast N: $N / $Nfinal..."
-		j=$(((N - Ninicio)/Npaso))
-
-		# Filtrar la línea que contiene el tiempo y seleccionar la
-		# tercera columna (el valor del tiempo). Dejar los valores en variables
-		# para poder imprimirlos en la misma línea del fichero de datos
 		fast1024=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast1024.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
 		fast2048=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast2048.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
 		fast4096=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast4096.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
 		fast8192=$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast8192.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
 
+    slowResults1024[$j]=$(cg_annotate cgout_slow1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
+		slowResults2048[$j]=$(cg_annotate cgout_slow2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
+		slowResults4096[$j]=$(cg_annotate cgout_slow4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
+		slowResults8192[$j]=$(cg_annotate cgout_slow8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
 		fastResults1024[$j]=$(cg_annotate cgout_fast1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
 		fastResults2048[$j]=$(cg_annotate cgout_fast2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
 		fastResults4096[$j]=$(cg_annotate cgout_fast4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
 		fastResults8192[$j]=$(cg_annotate cgout_fast8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5 $8}' | sed -e 's/,//g')
 
+		echo "$N	${slowResults1024[$i]}	${fastResults1024[$i]}" >> $fDAT1024
+	  echo "$N	${slowResults2048[$i]}	${fastResults2048[$i]}" >> $fDAT2048
+	  echo "$N	${slowResults4096[$i]}	${fastResults4096[$i]}" >> $fDAT4096
+	  echo "$N	${slowResults8192[$i]}	${fastResults8192[$i]}" >> $fDAT8192
+
 	done
-
-Nelementos=${#slowResults1024[*]}
-for ((i = 0 ; i < Nelementos ; i++)); do
-	N=$((Ninicio + i*Npaso))
-
-	echo "$N	${slowResults1024[$i]}	${fastResults1024[$i]}" >> $fDAT1024
-	echo "$N	${slowResults2048[$i]}	${fastResults2048[$i]}" >> $fDAT2048
-	echo "$N	${slowResults4096[$i]}	${fastResults4096[$i]}" >> $fDAT4096
-	echo "$N	${slowResults8192[$i]}	${fastResults8192[$i]}" >> $fDAT8192
-
-done
-
-
 
 echo "Generating plot..."
 # llamar a gnuplot para generar el gráfico y pasarle directamente por la entrada
