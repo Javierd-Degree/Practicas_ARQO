9831a82d7085a89c780ceb3221fd086163192992
javi <javier.lopezcano@estudiante.uam.es>
2018-11-19 20:38:30 +0100

    cambios

--- Practica3/Ej2.sh
+++ Practica3/Ej2.sh
@@ -11,56 +11,46 @@ fDAT4096=cache_4096.dat
 fDAT8192=cache_8192.dat
 fPNG1=slow_fast_read.png
 fPNG2=slow_fast_write.png
-slowResults1024=()
-slowResults2048=()
-slowResults4096=()
-slowResults8192=()
-fastResults1024=()
-fastResults2048=()
-fastResults4096=()
-fastResults8192=()
 
-# Borrar el fichero DAT y el fichero PNG
+# Borrar los ficheros DAT y PNG
 rm -f $fDAT1024 $fDAT2048 $fDAT4096 $fDAT8192 fPNG1 fPNG2
 
-# Generar el fichero DAT vacío
+# Generar los ficheros DAT vacíos
 touch $fDAT $fDAT2048 $fDAT4096 $fDAT8192
 
 echo "Running slow and fast..."
 
 	# Ejecutamos los programas slow y fast con todos los N
-	# varias veces, como se indica en el enunciado.
-	# Guardamos los resultados en un array para luego guardarlo
-	# en el fichero directamente
+	# una sola vez, como se nos indica.
 	for ((N = Ninicio ; N <= Nfinal ; N += Npaso)); do
 		echo "Slow N: $N / $Nfinal..."
-		j=$(((N - Ninicio)/Npaso))
 
-		# Filtrar la línea que contiene el tiempo y seleccionar la
-		# tercera columna (el valor del tiempo). Dejar los valores en variables
-		# para poder imprimirlos en la misma línea del fichero de datos
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow1024.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./slow $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow2048.dat --I1=2048,1,64 --D1=2048,1,64 --LL=8388608,1,64 ./slow $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow4096.dat --I1=4096,1,64 --D1=4096,1,64 --LL=8388608,1,64 ./slow $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow8192.dat --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 ./slow $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast1024.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast2048.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast4096.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
-		$(valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast8192.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N)
+		# Ejecutamos los programas con cachegrind para poder obtener los fallos de cache producidos
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow1024.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./slow $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow2048.dat --I1=2048,1,64 --D1=2048,1,64 --LL=8388608,1,64 ./slow $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow4096.dat --I1=4096,1,64 --D1=4096,1,64 --LL=8388608,1,64 ./slow $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_slow8192.dat --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 ./slow $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast1024.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast2048.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast4096.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N
+		valgrind --tool=cachegrind --cachegrind-out-file=cgout_fast8192.dat --I1=1024,1,64 --D1=1024,1,64 --LL=8388608,1,64 ./fast $N
 
-    	slowResults1024[$j]=$(cg_annotate cgout_slow1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		slowResults2048[$j]=$(cg_annotate cgout_slow2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		slowResults4096[$j]=$(cg_annotate cgout_slow4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		slowResults8192[$j]=$(cg_annotate cgout_slow8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		fastResults1024[$j]=$(cg_annotate cgout_fast1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		fastResults2048[$j]=$(cg_annotate cgout_fast2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		fastResults4096[$j]=$(cg_annotate cgout_fast4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
-		fastResults8192[$j]=$(cg_annotate cgout_fast8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		# Mediante grep y awk cogemos de los ficheros en los que se han guardado los resultados del cachegrind,
+		# los datos que representan los fallos de cache de lectura y de escritura y con sed quitamos las comas.
+    	slowResults1024=$(cg_annotate cgout_slow1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		slowResults2048=$(cg_annotate cgout_slow2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		slowResults4096=$(cg_annotate cgout_slow4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		slowResults8192=$(cg_annotate cgout_slow8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		fastResults1024=$(cg_annotate cgout_fast1024.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		fastResults2048=$(cg_annotate cgout_fast2048.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		fastResults4096=$(cg_annotate cgout_fast4096.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
+		fastResults8192=$(cg_annotate cgout_fast8192.dat | head -n 30 | grep 'TOTALS' | awk '{print $5"\t"$8}' | sed -e 's/,//g')
 
-		echo "$N	${slowResults1024[$j]}	${fastResults1024[$j]}" >> $fDAT1024
-		echo "$N	${slowResults2048[$j]}	${fastResults2048[$j]}" >> $fDAT2048
-		echo "$N	${slowResults4096[$j]}	${fastResults4096[$j]}" >> $fDAT4096
-		echo "$N	${slowResults8192[$j]}	${fastResults8192[$j]}" >> $fDAT8192
+		# Vamos imprimiendo los distintos datos en los ficheros DAT como se nos pide en el enunciado.
+		echo "$N	${slowResults1024}	${fastResults1024}" >> $fDAT1024
+		echo "$N	${slowResults2048}	${fastResults2048}" >> $fDAT2048
+		echo "$N	${slowResults4096}	${fastResults4096}" >> $fDAT4096
+		echo "$N	${slowResults8192}	${fastResults8192}" >> $fDAT8192
 
 	done
 
